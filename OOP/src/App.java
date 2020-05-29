bec4abf3685513bc5375d7f815ec12af7f088e82
Bevinda Juliani Enge <BE40202X@cimbniaga.co.id>
2020-05-29 08:35:51 +0700

    day 10

--- OOP/src/App.java
+++ OOP/src/App.java
@@ -1,17 +1,27 @@
 package OOP.src;
 
+import java.util.Scanner;
+import OOP.src.Employee;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-
-        int salary = 50_000;
-        int overtime = 5;
-        int payPerHour = 100;
-
-        System.out.println(calculateWage(salary, overtime, payPerHour));
+        Scanner scan = new Scanner(System.in);
+        Employee orang = new Employee(50_000, 100);
+        Employee person = new Employee(110_000, 30);
+        // System.out.print("Input gaji pokok anda: ");
+        // orang.setSalary(Integer.parseInt(scan.nextLine()));
+        // // System.out.print("Input lama overtime: ");
+        // // orang.setOvertime(Integer.parseInt(scan.nextLine()));
+        // System.out.print("Input gaji overtime: ");
+        // orang.setPayPerHour(Integer.parseInt(scan.nextLine()));
+        // System.out.println(calculateWage(orang.getSalary(), orang.getOvertime(), orang.getPayPerHour()));
+        // int overtime = scan.nextInt();
+        // System.out.println(orang.calculateWage(overtime));
+        // System.out.println(orang.calculateWage(5));
+        System.out.println(Employee.employeeCount);
     }
 
-    public static int calculateWage(int salary, int overtime, int payPerHour) {
-        return salary + (overtime * payPerHour);
-    }
+    // public static int calculateWage(int salary, int overtime, int payPerHour) {
+    //     return salary + (overtime * payPerHour);
+    // }
 }
